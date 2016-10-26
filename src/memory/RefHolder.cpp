6e3c54e5a4e073623eda8f07dd8f8be7ad1c2c17
Dawid Drozd <drozddawid.uam@gmail.com>
2016-10-26 14:29:13 +0200

    Add RefHolder<T>
    Simple Holder for cocos2dx::Ref class. Holder is using RAII for retain()
    and release() of managed object

---
+++ src/memory/RefHolder.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Dawid Drozd aka Gelldur on 26/10/16.
+//
+
+#include "RefHolder.h"
+
+
---
+++ src/memory/RefHolder.h
@@ -0,0 +1,146 @@
+//
+// Created by Dawid Drozd aka Gelldur on 26/10/16.
+//
+
+#pragma once
+
+#include <memory>
+#include <type_traits>
+
+#include <base/CCRef.h>
+
+namespace Dexode
+{
+
+/**
+ * RAII
+ */
+
+template<class T>
+class RefHolder
+{
+	static_assert(std::is_base_of<cocos2d::Ref, T>::value, "T must inherit from cocos2d::Ref");
+public:
+	RefHolder(T* ref)
+			: _ref(ref)
+	{
+		retain();
+	}
+
+	~RefHolder()
+	{
+		release();
+	}
+
+	RefHolder(RefHolder const& other)
+			: RefHolder(other._ref)
+	{
+	}
+
+	RefHolder(RefHolder&& other)
+			: _ref(other.reset())
+	{
+	}
+
+	RefHolder& operator=(RefHolder const& other)
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+
+		auto previous = _ref;
+
+		_ref = other._ref;
+		retain();
+
+		if (previous != nullptr)
+		{
+			previous->release();
+		}
+
+		return *this;
+	}
+
+	RefHolder& operator=(RefHolder&& other)
+	{
+		if (this == &other)
+		{
+			return *this;
+		}
+
+		if (_ref != nullptr && _ref == other._ref)
+		{
+			other.reset();
+			return *this;
+		}
+		auto previous = _ref;
+		_ref = other.reset();
+
+		if (previous != nullptr)
+		{
+			previous->release();
+		}
+
+		return *this;
+	}
+
+	T* reset()
+	{
+		auto ref = _ref;
+		_ref = nullptr;
+		return ref;
+	}
+
+	void release()
+	{
+		if (_ref != nullptr)
+		{
+			_ref->release();
+		}
+		_ref = nullptr;
+	}
+
+	T* autorelease()
+	{
+		if (_ref != nullptr)
+		{
+			_ref->autorelease();
+		}
+		auto ref = _ref;
+		_ref = nullptr;
+		return ref;
+	}
+
+	operator bool() const
+	{
+		return _ref != nullptr;
+	}
+
+	T* operator->() const
+	{
+		if (_ref == nullptr)
+		{
+			throw std::runtime_error("null pointer exception");
+		}
+		return _ref;
+	}
+
+	T* get() const
+	{
+		return _ref;
+	}
+
+private:
+	T* _ref = nullptr;
+
+	void retain()
+	{
+		if (_ref != nullptr)
+		{
+			_ref->retain();
+		}
+	}
+};
+
+}
