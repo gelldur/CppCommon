4ec52646a20205ddbd37e8f09d62d4319736b450
Dawid Drozd <drozddawid.uam@gmail.com>
2017-01-16 11:11:03 +0100

    Add ShowMyBoundOnScreen for debug purposes

---
+++ src/debug/ShowMyBoundsOnScreen.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Dawid Drozd aka Gelldur on 02/11/16.
+//
+
+#include "ShowMyBoundsOnScreen.h"
+
+#include <cocos2d.h>
+
+namespace Dexode
+{
+
+void ShowMy::boundsOnScreen(cocos2d::Node* node)
+{
+	auto drawNode = cocos2d::DrawNode::create(1);
+	node->addChild(drawNode, INT_MAX);
+	if (node->getScaleX() != 0)
+	{
+		drawNode->setScaleX(1.0 / node->getScaleX());
+	}
+	if (node->getScaleY() != 0)
+	{
+		drawNode->setScaleY(1.0 / node->getScaleY());
+	}
+
+	drawNode->schedule([node, drawNode](float dt)
+					   {
+						   drawNode->clear();
+
+						   auto rect = node->getBoundingBox();
+						   drawNode->drawRect({0, 0}, {rect.size.width, rect.size.height}
+											  , cocos2d::Color4F::MAGENTA);
+					   }, 0, "boundsOnScreenUpdate");
+
+	drawNode->setOnEnterCallback([node, drawNode]()
+								 {
+									 drawNode->clear();
+
+									 auto rect = node->getBoundingBox();
+									 drawNode->drawRect({0, 0}, {rect.size.width, rect.size.height}
+														, cocos2d::Color4F::MAGENTA);
+								 });
+}
+
+}
---
+++ src/debug/ShowMyBoundsOnScreen.h
@@ -0,0 +1,20 @@
+//
+// Created by Dawid Drozd aka Gelldur on 02/11/16.
+//
+
+#pragma once
+
+namespace cocos2d
+{
+class Node;
+}
+
+namespace Dexode
+{
+
+namespace ShowMy
+{
+void boundsOnScreen(cocos2d::Node* node);
+};
+
+}
